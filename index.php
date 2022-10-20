341da3f5550a51610881d513ac117d02f60e694c
RobertoRuizRuiz <robertooli98@gmail.com>
2022-10-20 10:38:36 +0200

    creacion del index

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    //require("asignaturas.php");
+    session_start();
+
+  
+    $vehiculos = array();
+
+
+    if (isset($_SESSION['arrayVehiculos']) == false) {
+        /* $vehiculo = new Vehiculo('Matricula1', 'Marca1', 'Modelo1', 'Color1', 'Revisado1');
+        array_push($vehiculos, $vehiculo);
+        $vehiculo = new Vehiculo('Matricula2', 'Marca2', 'Modelo2', 'Color2', 'Revisado2');
+        array_push($vehiculos, $vehiculo);
+        $vehiculo = new Vehiculo('Matricula3', 'Marca3', 'Modelo3', 'Color3', 'Revisado3');
+        array_push($vehiculos, $vehiculo);
+
+        $_SESSION['arrayVehiculos'] = $vehiculos; */
+    }
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instituto home</title>
+</head>
+<body>
+    <h2>Taller</h2>
+    <button><a href="listadoVehiculos.php">Vehiculos</a></button>
+</body>
+</html>
